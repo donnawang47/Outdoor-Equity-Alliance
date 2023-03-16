c470072ee86f9ecdaa0ee21842e40b29e1d6922c
Liz Garcia <lg6248@princeton.edu>
2023-03-16 17:08:13 -0400

    working on inserting content into students, programs, and modules tables

--- create_database.py
+++ create_database.py
@@ -1,29 +1,41 @@
-import sqlite3
-import contextlib
+import os
 import sys
+import psycopg2
 
 # run these commands in courselab sqlite3 mode.
 
-DATABASE_URL = 'file:reg.sqlite?mode=rwc'
+# DATABASE_URL = 'file:reg.sqlite?mode=rwc'
 
 def main():
 
 #! Index on tables? For admin, maybe program id...
     try:
-        with sqlite3.connect(DATABASE_URL, isolation_level = None,
-        uri = True) as connection:
-            with contextlib.closing(connection.cursor()) as cursor:
+        database_url = os.getenv('DATABASE_URL')
 
-                create_students_table = """ CREATE TABLE IF NOT EXISTS students (student_id INTEGER DEFAULT NULL, student_name TEXT DEFAULT NULL,
-                student_email TEXT DEFAULT NULL, enrollment TEXT DEFAULT NULL, completion TEXT DEFAULT NULL)"""
+        with psycopg2.connect(database_url) as connection:
+
+            with connection.cursor() as cursor:
+
+                cursor.execute("DROP TABLE IF EXISTS students")
+
+                 # programs: enrolled, available, locked
+                # program id: P*
+                # assessments: 0 for not complete, 1 for incomplete
+                # assessment id: a*
+
+                create_students_table = """ CREATE TABLE students (student_id INTEGER DEFAULT NULL, student_name TEXT DEFAULT NULL, student_email TEXT DEFAULT NULL, P1 TEXT DEFAULT NULL, A1 INTEGER DEFAULT 0)"""
 
                 cursor.execute(create_students_table)
 
-                create_programs_table = """ CREATE TABLE IF NOT EXISTS programs (program_id INTEGER DEFAULT NULL, program_name TEXT DEFAULT NULL, description TEXT DEFAULT NULL, initial_availability TEXT DEFAULT NULL)"""
+                cursor.execute("DROP TABLE IF EXISTS programs")
+
+                create_programs_table = """ CREATE TABLE programs (program_id INTEGER DEFAULT NULL, program_name TEXT DEFAULT NULL, description TEXT DEFAULT NULL, initial_availability TEXT DEFAULT NULL)"""
 
                 cursor.execute(create_programs_table)
 
-                create_modules_table = """ CREATE TABLE IF NOT EXISTS modules (module_id INTEGER DEFAULT NULL, program_name INTEGER DEFAULT NULL, module_name TEXT DEFAULT NULL, content_type TEXT DEFAULT NULL, content_link TEXT DEFAULT NULL, module_index INTEGER DEFAULT NULL)"""
+                cursor.execute("DROP TABLE IF EXISTS modules")
+
+                create_modules_table = """ CREATE TABLE modules (module_id INTEGER DEFAULT NULL, program_id INTEGER DEFAULT NULL, module_name TEXT DEFAULT NULL, content_type TEXT DEFAULT NULL, content_link TEXT DEFAULT NULL, module_index INTEGER DEFAULT NULL)"""
 
                 cursor.execute(create_modules_table)
 
--- insert_database.py
+++ insert_database.py
@@ -1,28 +1,65 @@
-import sqlite3
-import contextlib
+import psycopg2
 import sys
+import os
 
-DATABASE_URL = 'file:reg.sqlite?mode=rwc'
+DATABASE_URL = os.getenv('DATABASE_URL')
 
-def insert(data):
+def insert_module(data):
     try:
-        with sqlite3.connect(DATABASE_URL, isolation_level = None,
-        uri = True) as connection:
-            with contextlib.closing(connection.cursor()) as cursor:
+        with psycopg2.connect(DATABASE_URL) as connection:
+            with connection.cursor() as cursor:
 
-                insert_link_statement = """ INSERT INTO modules (module_id INTEGER, program_name INTEGER, module_name TEXT, content_type TEXT, content_link TEXT, module_index INTEGER) VALUES (?, ?, ?, ?, ?, ?)  """
+                # need to get module id
+
+                insert_link_statement = """ INSERT INTO modules (module_id, program_name, module_name, content_type, content_link, module_index) VALUES (%s, %s, %s, %s, %s, %s)  """
                 cursor.execute(insert_link_statement, data)
 
     except Exception as error:
             print(sys.argv[0] + ': ' + str(error), file=sys.stderr)
             sys.exit(1)
 
+def insert_program(data):
+    try:
+        with psycopg2.connect(DATABASE_URL) as connection:
+
+            with connection.cursor() as cursor:
+
+                # need to get program_id
+
+                stmt_str = """
+                INSERT INTO programs (program_id, program_name, description, initial_availability) VALUES (%s, %s, %s, %s)
+                """
+                cursor.execute(insert_link_statement, data)
+
+    except Exception as ex:
+        print(sys.argv[0] + ': ' + str(error), file=sys.stderr)
+        sys.exit(1)
+
+def insert_students(data):
+    try:
+        with psycopg2.connect(DATABASE_URL) as connection:
+            with connection.cursor() as cursor:
+
+                # need to get student id
+
+                insert_link_statement = """ INSERT INTO students (student_id, student_name, student_email) VALUES (%s, %s, %s, %s)  """
+                cursor.execute(insert_link_statement, data)
+
+    except Exception as error:
+            print(sys.argv[0] + ': ' + str(error), file=sys.stderr)
+            sys.exit(1)
+
+def add_program():
+
+
+
+
 def main():
      #! must pass in data to be inserted into modules table.
      data = [1, 'Module 1', 'lesson 1.1', 'google doc', 'https://docs.google.com/document/d/1QObJ5USYw30AdCjBgP9LFBSu8MTCZZqiB8jriQlzhF8/edit?usp=sharing', 1]
 
-     insert(data)
+     insert_module(data)
 
 
 if __name__ == '__main__':
-    main() 
+    main()
