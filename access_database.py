a26c133d78c562e231fd6dbd39035a1ea6268522
Donna Wang <dw3874@princeton.edu>
2023-03-21 02:16:59 -0400

    working get_all_programs, get_program_modules

--- access_database.py
+++ access_database.py
@@ -50,19 +50,29 @@ def get_program_modules(program_id):
 
                 cursor.execute(stmt_str, [program_id])
                 table = cursor.fetchall()
-                for row in table:
-                    print(row)
+                # for row in table:
+                #     print(row)
 
-                # list of dictionaries of programs
-                data = []
-                for row in table:
-                    data_row = {}
-                    data_row['program_id'] = row[0]
-                    data_row['program_name'] = row[1]
-                    data_row['description'] = row[2]
-                    data_row['program_availability'] = row[3]
-                    data.append(data_row)
 
+                data = {}
+                data['program_id'] = table[0][0]
+                data['program_name'] = table[0][1]
+                data['description'] = table[0][2]
+                data['program_availability'] = table[0][3]
+
+                # list of dictionaries of modules within program
+                modules = []
+                for row in table:
+                    modules_row = {}
+                    modules_row['module_id'] = row[4]
+                    # row[5] is program_id
+                    modules_row['module_name'] = row[6]
+                    modules_row['content_type'] = row[7]
+                    modules_row['content_link'] = row[8]
+                    modules_row['module_index'] = row[9]
+                    modules.append(modules_row)
+
+                data['modules'] = modules
                 return data
 
     except Exception as error:
@@ -90,6 +100,24 @@ def main():
     # how are we going to get id
     program_id = 'P1'
     modules = get_program_modules(program_id)
+
+    print("Program name:", modules['program_name'])
+    print("Program description:", modules['description'])
+    print("Program availability:", modules['program_availability'])
+    for module in modules['modules']:
+        print("MODULE:", module['module_name'], module['content_type'], module['content_link'], module['module_index'])
+    print("------------------------------------------------------")
+
+    print("Display modules for LOCKED PROGRAM")
+    # how are we going to get id
+    program_id = 'P2'
+    modules = get_program_modules(program_id)
+    print("Program name:", modules['program_name'])
+    print("Program description:", modules['description'])
+    print("Program availability:", modules['program_availability'])
+    for module in modules['modules']:
+        print("MODULE:", module['module_name'], module['content_type'], module['content_link'], module['module_index'])
+    print("------------------------------------------------------")
 
 if __name__ == '__main__':
     main()
